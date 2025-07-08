1ea88b14d7b51d6d66dd2c3ef808c82d88b616e5
Todor Gerganov P <todor.gerganov@gmail.com>
2025-07-08 14:28:41 +0200

    Add "sync" - publish and install apps

--- Scripts/MyTestAppInstall.ps1
+++ Scripts/MyTestAppInstall.ps1
@@ -1,4 +1,4 @@
 $AppFile = Get-ChildItem -Path C:\ProgramData\BcContainerHelper\Extensions\BCLatestDevOps\my\appProjectFolder\TestApp\output\* -Include *.app
 $Credential = [PSCredential]::new("admin", (ConvertTo-SecureString -String "Admin@123" -AsPlainText -Force))
 
-Publish-BcContainerApp -appFile $AppFile -containerName BCLatestDevOps -credential $Credential -install -SkipVerification
+Publish-BcContainerApp -appFile $AppFile -containerName BCLatestDevOps -credential $Credential -install -SkipVerification -sync
