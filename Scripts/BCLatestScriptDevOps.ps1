8fd6aca0123f5eee556213820474552817afef46
Todor Gerganov P <todor.gerganov@gmail.com>
2025-07-08 11:39:52 +0200

    Add PS scripts for container creation and publish/install apps - core + test

---
+++ Scripts/BCLatestScriptDevOps.ps1
@@ -0,0 +1,11 @@
+﻿$artifactUrl = Get-BcArtifactUrl -type sandbox -country de -select Latest
+$containerName = "BCLatestDevOps"
+$auth = "UserPassword"
+$credential = New-Object pscredential 'admin', (ConvertTo-SecureString -String 'Admin@123' -AsPlainText -Force)
+
+New-BCContainer -accept_eula -alwaysPull `
+ -artifactUrl $artifactUrl -containerName $containerName `
+ -auth $auth -credential $credential `
+ -assignPremiumPlan `
+ -updateHosts -includeAL `
+ -memoryLimit 4G
